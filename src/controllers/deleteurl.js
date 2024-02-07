6b31ceb7904ff6cfa18dca73dca2d702d4979624
yash kumar mishra <mishrayash3778@gmail.com>
2024-02-07 16:26:32 +0530

    hj

--- src/controllers/deleteurl.js
+++ src/controllers/deleteurl.js
@@ -1,10 +1,10 @@
 import deleteurlById from '../db/url.model.js'
-
+import URLModel from "../db/url.model.js";
 
 export const deleteurl = async (req, res) => {
     try {
       const { id } = (req.params);
-      const deletedUser = await deleteurlById(id);
+      const deletedUser = await URLModel.findOneAndDelete({ _id: id });
   
       return res.json(deletedUser);
     } catch (error) {
